6700a99eab07168b97073b9b4af457a3c69ca67d
RogovayaaElizaveta <elizavetarogovaya2005@gmail.com>
2025-05-29 12:38:05 +0300

    Fix: re-added 22nd lesson as a regular folder, not submodule

---
+++ Twentyth two lesson Hillel home work/src/index.js
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import './styles.scss';
+
+const input = document.getElementById("taskInput");
+const addButton = document.getElementById("addTask");
+const list = document.getElementById("taskList");
+const modalText = document.getElementById("modalTaskText");
+
+addButton.addEventListener("click", () => {
+  const text = input.value;
+  if (text !== "") {
+    const newItem = document.createElement("li");
+    newItem.className = "list-group-item task-item";
+    newItem.textContent = text;
+
+    newItem.addEventListener("click", () => {
+      modalText.textContent = text;
+      const myModal = new bootstrap.Modal(document.getElementById("taskModal"));
+      myModal.show();
+    });
+
+    list.appendChild(newItem);
+  }
+});
---
+++ Twentyth two lesson Hillel home work/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.[contenthash].js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.[contenthash].css',
+    }),
+  ],
+  devServer: {
+    static: './public',
+    port: 8080,
+    open: true,
+  },
+  mode: 'development', // або 'production'
+};
